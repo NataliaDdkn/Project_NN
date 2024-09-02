e13844ce4e19ec48a50321871b38fae97f9e2230
NataliaDdkn <dedikina@biocad.ru>
2024-09-02 17:15:58 +0300

    commit_4

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Ya sdelal!'
+#@app.route('/')
+#def enter():
+    #return redirect(f"/submit")
+
+@app.route('/submit', methods=['GET', 'POST'])
+def handle_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return redirect(f"/thank_you?name={name}")
+    return render_template('form.html')
+
+@app.route('/thank_you')
+def thank_you():
+    name = request.args.get('name')
+    return f"Thank you, {name}, for submitting the form!"
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run()
