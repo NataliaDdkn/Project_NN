5538fcf42aecaf571371a27eeed17d28cb180db9
290141natasha <kuliushina@biocad.ru>
2024-09-06 16:09:19 +0300

    new7

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
+from models import CARDS, db
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# связываем приложение и экземпляр SQLAlchemy
+db.init_app(app)
+#создаем все, что есть в db.Models
+with app.app_context():
+    db.create_all()
 import requests
 import json
 app.config['SECRET_KEY']= 'ochen_$ecRetNyI_Kod'
@@ -9,7 +19,22 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_THRESHOLD'] = 500
 Session(app)
 
+def ...():
 
+ new_card = get_card(...)['cards'][0]
+ # создаем экземпляр модели
+ new_card_to_db = CARDS (
+     deck_id = session.get('deck_id'),
+       username = session.get('username'),
+       datetime = datetime.now(),
+       card = new_card['code'])
+
+ # добавляем экземпляр в сессию базы данных
+ db.session.add(new_card_to_db)
+ # подтверждаем изменения сессии и сохраняем данные
+  db.session.commit()
+
+return ...
 
 @app.route('/')
 def get_deck():
@@ -49,7 +74,7 @@ def get_cards(deck):
 	
 	session["points"].append(card_points)
 	points_all=sum(session["points"])
-		
+	
 	cards_left = json.loads(
 	requests.post('https://deckofcardsapi.com/api/deck/'+deck+'').text)['remaining']
 	return render_template('draw.html', my_cards=session["my_cards"], deck=deck, cards_left=cards_left, points_all=points_all)
